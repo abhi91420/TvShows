f230e15698ddf2ebbd8c35d67c9606b51243b642
abhi91420@gmail.com <Hitler(9>
2022-06-28 20:42:11 -0400

    added README

--- TvShows/controllers/controller_show.py
+++ TvShows/controllers/controller_show.py
@@ -16,3 +16,4 @@ def show_one_tvshow(id):
     show = Show.show_info(data)
     user = Index.get_user_by_id({'id': session['user_id']})
     return render_template('show.html', recipe=show, user=user)
+
